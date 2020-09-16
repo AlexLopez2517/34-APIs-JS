fa53b3a80a141434648ea2003f13bfb1dc51b13d
AlexLopez2517 <alex2761086444@gmail.com>
2020-09-15 22:50:27 -0500

    Update 04-app.js
    Video 216

---
+++ js/04-app.js
@@ -0,0 +1,13 @@
+const abrirBtn = document.querySelector('#abrir-pantalla-completa');
+const salirBtn = document.querySelector('#salir-pantalla-completa');
+
+abrirBtn.addEventListener('click', pantallaCompleta);
+salirBtn.addEventListener('click', cerrarPantallaCompleta);
+
+function pantallaCompleta(){
+    document.documentElement.requestFullscreen();
+}
+
+function cerrarPantallaCompleta(){
+    document.exitFullscreen();
+}
