a95c5cc03503c0d30d3ff238dfb645b617395e1f
AlexLopez2517 <alex2761086444@gmail.com>
2020-09-15 20:59:07 -0500

    Update 01-app.js
    Es el primer commit del video 213 C:

---
+++ js/01-app.js
@@ -0,0 +1,24 @@
+const notificarBtn = document.querySelector('#notificar');
+
+notificarBtn.addEventListener('click', () => {
+        Notification
+            .requestPermission()
+            .then( resultado => {
+                console.log('El resultado es ', resultado);
+            })
+    });
+
+    const verNotificacion = document.querySelector('#verNotificacion');
+    verNotificacion.addEventListener('click', () => {
+       if(Notification.permission === 'granted') {
+           const notificacion = new Notification('Esta es la notificación',{
+               icon: 'img/ccj.png',
+               body: 'Codigo con Juan, aprende con proyectos reales'
+           })
+
+           notificacion.onclick = function(){
+               window.open('https://www.codigoconjuan.com')
+           }
+       } 
+
+    });
