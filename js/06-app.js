ddff77abe8e5ebce045fc9af08fc08822a1b6c75
AlexLopez2517 <alex2761086444@gmail.com>
2020-09-16 21:17:07 -0500

    218

---
+++ js/06-app.js
@@ -0,0 +1,38 @@
+const salida = document.querySelector('#salida');
+const microfono = document.querySelector('#microfono');
+
+microfono.addEventListener('click', ejecutarSpeechAPI);
+
+function ejecutarSpeechAPI() {
+    const SpeechRecognition = webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.start();
+
+    recognition.onstart = function() {
+        salida.classList.add('mostrar');
+        salida.textContent = 'Escuchando...';
+
+    };
+
+    recognition.onspeechend = function() {
+        salida.textContent = 'Se dejo de grabar...';
+        recognition.stop();
+    };
+
+    recognition.onresult = function(e) {
+        console.log(e.results[0][0]);
+
+        const {confidence, transcript} = e.results[0][0];
+
+        const speech = document.createElement('p');
+        speech.innerHTML = 'Grabado: ${transcript}';
+
+        const seuridad = document.createElement('p');
+        seguridad.innerHTML = 'seguridad: ${ parseInt( confidence = 100 ) }';
+
+        salida.appendChild(speech);
+        salida.appendChild(seguridad);
+
+    }
+}
